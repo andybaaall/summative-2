9650b334e0286bcf9dde989079043b9b1ebbfe1f
Andy Ball <932400399@bun2-206-11.local>
2019-07-02 15:31:21 +1200

    abt to start working on party validation function

---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  $('#partyDetails').hide();
+  $('#topRightLogo').hide();
+  $('#partyNext').hide();
+
+
+  // let's go btn
+
+  $('#letsGo').click(function(){
+    $('#landingPage').hide();
+    $('#partyDetails').show();
+    $('#topRightLogo').show();
+  })
+
+  // party details validation
+
+  // temp
+
+
+})
