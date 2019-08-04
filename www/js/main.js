b983d02c237cc78830198cb7a7c3ea53a82af9b0
Andy Ball <932400399@bun2-206-11.local>
2019-08-04 19:50:04 +1200

    linted JS, validated HTML, added loadingOverlay.js

--- www/js/main.js
+++ www/js/main.js
@@ -4,54 +4,46 @@ var fuelCost = 2.269;
 // all vehicles
 var cars = [
   {
-    name: 'Motorbike',
-    id: 'Motorbike',
-    minPeople: 1,
-    maxPeople: 1,
-    minDays: 1,
-    maxDays: 5,
-    fuel: 3.7,
-    // fuel is a measurement of L / km
-    hire: 109
-    // hire is a measurement of $ / day
+    name : 'Motorbike',
+    id : 'Motorbike',
+    minPeople : 1,
+    maxPeople : 1,
+    minDays : 1,
+    maxDays : 5,
+    fuel : 3.7, // L / 100km
+    hire : 109 // $ / day
   },
   {
-    name: 'Small car',
-    id: 'SmallCar',
-    minPeople: 1,
-    maxPeople: 2,
-    minDays: 1,
-    maxDays: 10,
-    fuel: 8.5,
-    // fuel is a measurement of L / km
-    hire: 129
-    // hire is a measurement of $ / day
+    name : 'Small car',
+    id : 'SmallCar',
+    minPeople : 1,
+    maxPeople : 2,
+    minDays : 1,
+    maxDays : 10,
+    fuel : 8.5, // L / 100km
+    hire : 129 // $ / day
   },
   {
-    name: 'Large car',
-    id: 'LargeCar',
-    minPeople: 1,
-    maxPeople: 5,
-    minDays: 1,
-    maxDays: 10,
-    fuel: 9.7,
-    // fuel is a measurement of L / km
-    hire: 144
-    // hire is a measurement of $ / day
+    name : 'Large car',
+    id : 'LargeCar',
+    minPeople : 1,
+    maxPeople : 5,
+    minDays : 1,
+    maxDays : 10,
+    fuel : 9.7, // L / 100km
+    hire : 144 // $ / day
   },
   {
-    name: 'Motor home',
-    id: 'MotorHome',
-    minPeople: 2,
-    maxPeople: 6,
-    minDays: 2,
-    maxDays: 15,
-    fuel: 17,
-    // fuel is a m.easurement of L / km
-    hire: 144
-    // hire is a measurement of $ / day
+    name : 'Motor home',
+    id : 'MotorHome',
+    minPeople : 2,
+    maxPeople : 6,
+    minDays : 2,
+    maxDays : 15,
+    fuel : 17, // L / 100km
+    hire : 144 // $ / day
   }
-]
+];
 
 // vehicles available to the user
 var availableCars = [];
@@ -60,7 +52,7 @@ var availableCars = [];
 $('#letsGo').click(function(){
   $('#landingPage').addClass('d-none');
   $('#partyDetails').removeClass('d-none');
-})
+});
 
 // party details validation
 var people = document.getElementById('people');
@@ -75,7 +67,7 @@ function validate(owner , min , max){
   if (val % 1 != 0){
     $('#partyError').removeClass('d-none');
     $('#partyErrorMsg').html('Please enter a whole number between ' + (min + 1) + ' and ' + (max - 1) + ' in the \'how many ' + owner.id + '\' field.');
-  }
+  };
 
   if (val <= min || val >= max) {
     $('#partyError').removeClass('d-none');
@@ -85,25 +77,25 @@ function validate(owner , min , max){
       hasPeople = true;
     } else {
       hasDays = true;
-    }
-  }
+    };
+  };
   if (hasPeople === true && hasDays === true){
     $('#partyNext').removeClass('d-none');
-  }
-}
+  };
+};
 
 people.addEventListener('change', function(){
   validate(people , 0 , 7);
-})
+});
 
 days.addEventListener('change', function(){
   validate(days , 0 , 16);
-})
+});
 
 // closes party details error message
 $('#partyErrorClose').click(function(){
   $('#partyError').addClass('d-none');
-})
+});
 
 // determines which cars are available based on party details
 document.getElementById('partyNext').addEventListener('click', function(){
@@ -112,14 +104,14 @@ document.getElementById('partyNext').addEventListener('click', function(){
       availableCars.push(cars[i]);
       $('#partyDetails').addClass('d-none');
       $('#mapDetails').removeClass('d-none');
-    }
-  }
+    };
+  };
   if (availableCars.length === 0){
     $('#partyNext').addClass('d-none');
     $('#partyError').removeClass('d-none');
     $('#partyErrorMsg').html('Sorry, but we can\'t find any vehicles that match your trip.');
-  }
-})
+  };
+});
 
 // map!
 
@@ -137,7 +129,7 @@ function initMap(){
     lng: 173.283966
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 6, center: center});
+  var map = new google.maps.Map(document.getElementById('map'), {zoom : 6, center : center});
 
   var startInput = document.getElementById('startInput');
   var endInput = document.getElementById('endInput');
@@ -145,15 +137,16 @@ function initMap(){
       types: ['address'],
       componentRestrictions: {country: 'nz'},
     };
+
   var startAutocomplete = new google.maps.places.Autocomplete(startInput, options);
   var endAutocomplete = new google.maps.places.Autocomplete(endInput, options);
 
   if (markerA == null) {
     markerA = new google.maps.Marker({map: map});
-  }
+  };
   if (markerB == null) {
     markerB = new google.maps.Marker({map: map});
-  }
+  };
 
   startAutocomplete.bindTo('bounds', map);
   endAutocomplete.bindTo('bounds', map);
@@ -167,9 +160,9 @@ function initMap(){
       map.panTo(place.geometry.location);
       if (markerA.position && markerB.position) {
         getDirections();
-      }
-    })
-  }
+      };
+    });
+  };
 
   setMarker(markerA , startAutocomplete);
   setMarker(markerB , endAutocomplete);
@@ -178,7 +171,7 @@ function initMap(){
   function getDirections(){
     if (directionsDisplay) {
       directionsDisplay.setMap(null);
-    }
+    };
      var directionsService = new google.maps.DirectionsService();
      directionsDisplay = new google.maps.DirectionsRenderer({});
      directionsDisplay.setMap(map);
@@ -196,15 +189,15 @@ function initMap(){
          } else {
            $('#mapConfirm').addClass('d-none');
            $('#mapError').removeClass('d-none');
-           $('#mapErrorMsg').html('error msg');
+           $('#mapErrorMsg').html('Sorry, but we couldn\'t find any routes for that journey!');
            $('#mapErrorClose').click(function(){
-             $('#mapError').addClass('Sorry, but we couldn\'t find any routes for that journey!');
-           })
-         }
-     })
+             $('#mapError').addClass('d-none');
+           });
+         };
+     }); // directionsService
     $('#mapConfirm').removeClass('d-none');
-   } // getDirections()
-}// initMap
+  }; // getDirections
+};// initMap
 
 // show vehicles page
 document.getElementById('mapConfirm').addEventListener('click', function(){
@@ -214,13 +207,13 @@ document.getElementById('mapConfirm').addEventListener('click', function(){
   for (var i = 0; i < availableCars.length; i++) {
     document.getElementById('vehiclesList').innerHTML += '<div class="row"><btn id="' + availableCars[i].id + '" class="btn btn-primary btn-round my-2" onclick="carSelect(this)"> ' + availableCars[i].name + '<br> $' + availableCars[i].hire + ' / day' + '<br>' + availableCars[i].fuel + ' L / 100km </btn></div>';
   } // availableCars loop
-})
+});
 
 var chosenCar;
 
-function carSelect(e){
+function carSelect(element){
   for (var i = 0; i < availableCars.length; i++) {
-    if (e.id == availableCars[i].id) {
+    if (element.id == availableCars[i].id) {
       chosenCar = [];
       chosenCar.push({
         name: availableCars[i].name,
@@ -231,14 +224,20 @@ function carSelect(e){
         maxDays: availableCars[i].maxDays,
         fuel: availableCars[i].fuel,
         hire: availableCars[i].hire
-      }) // push()
-    } // if
-  } // for loop
+      }); // push()
+    }; // if
+  }; // for loop
   $('#vehicleDetails').addClass('d-none');
-  $('#results').removeClass('d-none');
-  $('#results').html('<div class="row">driving distance is ' + distance + '</div><div class="row">driving time is ' + time + '</div> <div class="row">fuel cost is $' + Math.round(((parseInt(distance) / 100) * chosenCar[0].fuel * fuelCost)) + '.00</div><div class="row">hire cost is $' + (chosenCar[0].hire * days.value) + '.00</div><div class="row"></div>');
-  $('#newTripBtn').removeClass('d-none');
-} // carSelect()
+
+  var spinHandle = loadingOverlay().activate();
+
+  setTimeout(function() {
+    loadingOverlay().cancel(spinHandle);
+    $('#results').removeClass('d-none');
+    $('#results').html('<div class="row">driving distance is ' + distance + '</div><div class="row">driving time is ' + time + '</div> <div class="row">fuel cost is $' + Math.round(((parseInt(distance) / 100) * chosenCar[0].fuel * fuelCost)) + '.00</div><div class="row">hire cost is $' + (chosenCar[0].hire * days.value) + '.00</div><div class="row"></div>');
+    $('#newTripBtn').removeClass('d-none');
+  }, 1500);
+}; // carSelect()
 
 // 'loading' screen
 
@@ -264,11 +263,11 @@ function newTrip(){
   markerA.position = null;
   markerB.position = null;
   initMap();
-}
+};
 
 document.getElementById('newTripBtn').addEventListener('click', function(){
   newTrip();
-})
+});
 
-// replaces the async , defer and &callback from the HTML
-google.maps.event.addDomListener(window, "load", initMap);
+// replaces the async , defer and &callback from the script tag
+google.maps.event.addDomListener(window, 'load', initMap);
